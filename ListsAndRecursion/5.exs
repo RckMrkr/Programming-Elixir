395f309adc2415f7c4bc648c055c49ce28f22a1e
Rick <rick@purplecow.dk>
2016-09-15 20:15:27 +0200

    ListAndRecursion-5 part 2

--- ListsAndRecursion/5.exs
+++ ListsAndRecursion/5.exs
@@ -1,7 +1,8 @@
 defmodule MyEnum do
 	
+	def all?(list, fun \\ fn x -> x end)
 	def all?([], _), do: true
-	def all?([head|tail], fun \\ fn x -> x end) do
+	def all?([head|tail], fun) do
 		fun.(head) && all?(tail)
 	end
 
@@ -11,8 +12,9 @@ defmodule MyEnum do
 		each(tail, fun)
 	end
 
+	def filter(list, fun \\ fn x -> x end)
 	def filter([], _), do: []
-	def filter([head|tail], fun \\ fn x -> x end) do
+	def filter([head|tail], fun) do
 		if(fun.(head)) do
 			[head|filter(tail, fun)]
 		else
@@ -20,17 +22,31 @@ defmodule MyEnum do
 		end
 	end
 
+	def split(enumerable, count) when count < 0 do
+		count = enumerable
+			|> length
+			|> (&(&1 + count)).()
+			|> max(0)
+		split(enumerable, count)
+	end
 	def split(enumerable, count) do
-		{ take(enumerable, count), take(enumerable, count - length(enumerable)) }
+		first = take(enumerable, count)
+		{ first, enumerable -- first }
 	end
 
+	defp _reverse([], acc), do: acc
+	defp _reverse([head|tail], acc), do: _reverse(tail, [head|acc])
+	defp reverse(enumerable), do: _reverse(enumerable, [])
+	
 	def take(_, 0), do: []
 	def take([], _), do: []
 	def take(list, count) when count < 0 do
-		# Todo
+		list
+		|> reverse
+		|> take(count * -1)
+		|> reverse
 	end
 	def take([head|tail], count) do
 		[head|take(tail, count - 1)]
 	end
-
 end
